a4ef9430e8fb919074c46c00a00e08fc0b8b495e
EmilMustafaev <emilmustafaev891@gmail.com>
2024-06-17 16:29:02 +0300

    1. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -0,0 +1,53 @@
+package ru.job4j.generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        Generics generics = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
+
+        generics.printObject(first);
+        generics.printObject(second);
+        generics.printObject(third);
+        System.out.println();
+
+
+        //generics.printBoundedWildCard(first);
+        generics.printBoundedWildCard(second);
+        generics.printBoundedWildCard(third);
+        System.out.println();
+
+        generics.printLowerBoundedWildCard(first);
+        generics.printLowerBoundedWildCard(second);
+        //generics.printLowerBoundedWildCard(third);
+    }
+
+    public void printObject(List<? extends Object> list) {
+        for (Iterator<? extends Object> iterator = list.iterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Iterator<? extends Predator> iterator = list.iterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
+        for (Iterator<? super Predator> iterator = list.iterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+}
