172ed2aed1d4ff0076192e26e8e10afe89bc561b
EmilMustafaev <emilmustafaev891@gmail.com>
2024-07-07 22:16:59 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,14 +9,34 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+
+    public static void main(String[] args) throws IOException {
+        validateArgs(args);
+
+        Path start = Paths.get(args[0]);
+        String fileExtension = args[1];
+        search(start, path -> path.toFile().getName().endsWith(fileExtension)).forEach(System.out::println);
+    }
+
+    public static void validateArgs(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder or file extension is null. Usage: ROOT_FOLDER FILE_EXTENSION.");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Invalid file extension format. It should start with a dot.");
+        }
+    }
+
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
-    }
-
-    public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".properties")).forEach(System.out::println);
     }
 }
