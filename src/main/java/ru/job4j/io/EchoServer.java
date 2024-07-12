c25cbcf2888a992370eaf11590c0379bad093feb
EmilMustafaev <emilmustafaev891@gmail.com>
2024-07-12 21:59:28 +0300

    2. Что такое Socket? [#4850 #504843]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,44 +1,29 @@
 package ru.job4j.io;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
+                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String firstLine = input.readLine();
+                    if (firstLine.contains("msg=Bye")) {
+                        server.close();
+                    }
+                    System.out.println(firstLine);
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                        if (string.contains("msg=Exit")) {
-                            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                            output.write("Server is closed".getBytes());
-                            server.close();
-                        }
-                        if (string.contains("msg=Any")) {
-                            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                            output.write("What".getBytes());
-                        }
-                        if (string.contains("msg=Hello")) {
-                            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                            output.write("Hello".getBytes());
-                        }
                         System.out.println(string);
                     }
                     output.flush();
                 }
             }
-        } catch (IOException e) {
-            LOG.error("Error", e);
         }
-
     }
 }
