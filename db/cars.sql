fa2d334202e3778522a092772fcf4437ca8d572b
EmilMustafaev <emilmustafaev891@gmail.com>
2024-07-26 11:51:22 +0300

    0. Что такое база данных. [#6860]

---
+++ db/cars.sql
@@ -0,0 +1,9 @@
+
+insert into cars(engine_capacity, model, is_sedan) values(1.8, 'BMW', true);
+select * from cars;
+
+update cars set model = 'Mersedes-Benz';
+select * from cars;
+
+delete from cars;
+select * from cars;
