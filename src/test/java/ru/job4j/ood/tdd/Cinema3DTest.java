d1bced40c814d30e7f4eeba4e054292ca4d12a76
EmilMustafaev <emilmustafaev891@gmail.com>
2024-09-10 16:14:56 +0300

    3. Что такое TDD? [#4918 #512658]

--- src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
+++ src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Calendar;
 import java.util.List;
-
+@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class Cinema3DTest {
     @Test
     public void whenBuyThenGetTicket() {
